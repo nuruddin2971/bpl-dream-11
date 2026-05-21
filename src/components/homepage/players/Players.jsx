30792535727ef5649d631d83a482a996d0dc6d3e
nuruddin2971 <nuruddin2971@gmail.com>
2026-05-21 22:53:28 +0800

    added all players photo

---
+++ src/components/homepage/players/Players.jsx
@@ -0,0 +1,10 @@
+import React, { use } from "react";
+
+const Players = ({ playersPromise }) => {
+  console.log(playersPromise);
+  const data = use(playersPromise);
+  console.log(data);
+  return <div>Players : {data.length}</div>;
+};
+
+export default Players;
